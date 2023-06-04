c5ffa6ddb5c965e923eb0c968d60f26371bbb075
s4njk4n <105919271+s4njk4n@users.noreply.github.com>
2023-06-04 17:30:41 +1000

    Update rpc_schedule.sh

--- rpc_schedule.sh
+++ rpc_schedule.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
 
-# Interactive menu
-echo "Select an option:"
-echo "1. Schedule 'rpc_check.sh' to run once per minute"
-echo "2. Cancel the minutely running of 'rpc_check.sh'"
-read -p "Option: " option
+# Check if running as root, otherwise run with sudo
+if [[ $EUID -ne 0 ]]; then
+    SUDO="sudo"
+fi
 
-case $option in
-  1)
-    # Schedule 'rpc_check.sh' to run once per minute
-    echo "*/1 * * * * $(pwd)/rpc_check.sh >> rpc_check.log" | crontab -
-    echo "Scheduled 'rpc_check.sh' to run once per minute"
-    ;;
-  2)
-    # Cancel the minutely running of 'rpc_check.sh'
-    crontab -l | grep -v "$(pwd)/rpc_check.sh" | crontab -
-    echo "Cancelled the minutely running of 'rpc_check.sh'"
-    ;;
-  *)
-    echo "Invalid option"
-    ;;
-esac
+# Function to start rpc_check.sh as a background process
+start_rpc_check() {
+    $SUDO chmod +x ~/XDC_RPC_Check/rpc_check.sh
+    $SUDO nohup ~/XDC_RPC_Check/rpc_check.sh >/dev/null 2>&1 &
+    echo "rpc_check.sh started running once per minute."
+}
+
+# Function to stop rpc_check.sh background process
+stop_rpc_check() {
+    $SUDO pkill -f "rpc_check.sh"
+    echo "rpc_check.sh stopped running."
+}
+
+# Interactive menu
+while true; do
+    echo "Please select an option:"
+    echo "1. Start running rpc_check.sh once per minute"
+    echo "2. Stop running rpc_check.sh"
+    read -p "Enter your choice (1 or 2): " choice
+
+    case $choice in
+    1)
+        start_rpc_check
+        break
+        ;;
+    2)
+        stop_rpc_check
+        break
+        ;;
+    *)
+        echo "Invalid choice. Please try again."
+        ;;
+    esac
+done
