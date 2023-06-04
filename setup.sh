1c04390dad51b2c0984120ac7d487cb1d3f346a9
s4njk4n <105919271+s4njk4n@users.noreply.github.com>
2023-06-04 17:48:19 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,47 +1,65 @@
 #!/bin/bash
 
-# Check if running as root, otherwise run with sudo
+# Check if sudo is required
 if [[ $EUID -ne 0 ]]; then
     SUDO="sudo"
 fi
 
-# Function to ask user for input and store in settings.txt
-ask_and_store() {
-    local prompt="$1"
-    local variable_name="$2"
-    
-    read -p "$prompt: " user_input
-    echo "$variable_name=$user_input" >>settings.txt
-}
-
 # Install dependencies
-$SUDO apt-get update
-$SUDO apt-get install -y curl
+$SUDO apt update
+$SUDO apt install -y curl ufw
 
-# Install and configure mail service
-$SUDO apt-get install -y mailutils
-$SUDO DEBIAN_FRONTEND=noninteractive apt-get install -y postfix
+# Install mail server components
+$SUDO apt install -y postfix mailutils
 
-# Install ufw
-$SUDO apt-get install -y ufw
+# Configure Postfix as an Internet Site
+echo "postfix postfix/mailname string $(hostname)" | $SUDO debconf-set-selections
+echo "postfix postfix/main_mailer_type string 'Internet Site'" | $SUDO debconf-set-selections
+$SUDO $SUDO dpkg-reconfigure -f noninteractive postfix
 
-# Perform necessary configurations for mail service
-$SUDO sed -i 's/inet_interfaces = all/inet_interfaces = loopback-only/' /etc/postfix/main.cf
-$SUDO systemctl restart postfix
+# Configure Postfix settings
+$SUDO postconf -e "inet_interfaces = loopback-only"
+$SUDO postconf -e "home_mailbox = Maildir/"
+$SUDO postconf -e "smtpd_banner = \$myhostname ESMTP"
+$SUDO postconf -e "alias_maps = hash:/etc/aliases"
+$SUDO postconf -e "alias_database = hash:/etc/aliases"
+$SUDO postconf -e "mydestination = \$myhostname, localhost.\$mydomain, localhost"
+$SUDO postconf -e "mynetworks = 127.0.0.0/8"
+$SUDO postconf -e "mailbox_size_limit = 0"
+$SUDO postconf -e "recipient_delimiter = +"
+$SUDO postconf -e "inet_protocols = ipv4"
 
-# Configure ufw to allow mail server access from localhost
+# Configure ufw
 $SUDO ufw allow from 127.0.0.1 to any port 25
+$SUDO ufw deny from any to any port 25
+$SUDO ufw enable
 
-# Configure ufw to block external access to mail server
-$SUDO ufw deny 25
+# Ask for RPC server IP address
+read -p "Enter the RPC server IP address: " RPC_IP
 
-# Ask user for RPC server IP address and email address for alerts
-ask_and_store "Enter the IP address of the RPC server" "RPC_IP"
-ask_and_store "Enter the email address to receive alerts" "EMAIL_ADDRESS"
+# Ask for alert email address
+read -p "Enter the email address for alert notifications: " ALERT_EMAIL
+
+# Determine if RPC endpoint uses SSL/TLS encryption
+read -p "Does the RPC endpoint use SSL/TLS encryption? (Y/N): " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    RPC_URL="https://${RPC_IP}"
+else
+    RPC_URL="http://${RPC_IP}"
+fi
+
+# Store user-defined data fields in settings.txt
+cat <<EOF >settings.txt
+RPC_IP=${RPC_IP}
+ALERT_EMAIL=${ALERT_EMAIL}
+RPC_URL=${RPC_URL}
+EOF
 
 # Restart services
-$SUDO systemctl restart postfix
-$SUDO ufw enable
+$SUDO service postfix restart
 
 # Create rpc_check.log file
 touch rpc_check.log
+
+echo "Setup completed successfully."
