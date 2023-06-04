e1ab57f920a29adb6f0d60ec0caab0d7aa25ecf4
s4njk4n <105919271+s4njk4n@users.noreply.github.com>
2023-06-04 17:31:14 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,38 +1,47 @@
 #!/bin/bash
 
-# Install dependencies
-echo "Installing dependencies..."
-apt update
-apt install -y curl ssmtp
+# Check if running as root, otherwise run with sudo
+if [[ $EUID -ne 0 ]]; then
+    SUDO="sudo"
+fi
 
-# Install local mail service (Postfix)
-echo "Installing local mail service..."
-DEBIAN_FRONTEND=noninteractive apt install -y postfix
+# Function to ask user for input and store in settings.txt
+ask_and_store() {
+    local prompt="$1"
+    local variable_name="$2"
+    
+    read -p "$prompt: " user_input
+    echo "$variable_name=$user_input" >>settings.txt
+}
+
+# Install dependencies
+$SUDO apt-get update
+$SUDO apt-get install -y curl
+
+# Install and configure mail service
+$SUDO apt-get install -y mailutils
+$SUDO DEBIAN_FRONTEND=noninteractive apt-get install -y postfix
 
 # Install ufw
-echo "Installing ufw..."
-apt install -y ufw
+$SUDO apt-get install -y ufw
 
-# Configure local mail service
-echo "Configuring local mail service..."
-sed -i 's/inet_interfaces = all/inet_interfaces = loopback-only/' /etc/postfix/main.cf
-service postfix restart
+# Perform necessary configurations for mail service
+$SUDO sed -i 's/inet_interfaces = all/inet_interfaces = loopback-only/' /etc/postfix/main.cf
+$SUDO systemctl restart postfix
 
-# Configure ufw to allow only local access to mail service
-echo "Configuring ufw..."
-ufw allow from 127.0.0.1 to any port 25
-ufw enable
+# Configure ufw to allow mail server access from localhost
+$SUDO ufw allow from 127.0.0.1 to any port 25
 
-# User input: RPC server IP address
-read -p "Enter the RPC server IP address: " rpc_server_ip
+# Configure ufw to block external access to mail server
+$SUDO ufw deny 25
 
-# User input: Email address for alerts
-read -p "Enter the email address for alerts: " email_address
+# Ask user for RPC server IP address and email address for alerts
+ask_and_store "Enter the IP address of the RPC server" "RPC_IP"
+ask_and_store "Enter the email address to receive alerts" "EMAIL_ADDRESS"
 
-# Store user-defined data fields in settings.txt
-echo "rpc_server_ip=$rpc_server_ip" > settings.txt
-echo "email_address=$email_address" >> settings.txt
+# Restart services
+$SUDO systemctl restart postfix
+$SUDO ufw enable
 
+# Create rpc_check.log file
 touch rpc_check.log
-
-echo "Setup completed successfully!"
