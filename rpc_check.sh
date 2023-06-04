9485304d89fd1d17f4312bab20a431d1e8fe9be1
s4njk4n <105919271+s4njk4n@users.noreply.github.com>
2023-06-04 17:08:31 +1000

    Update rpc_check.sh

--- rpc_check.sh
+++ rpc_check.sh
@@ -1,42 +1,27 @@
 #!/bin/bash
 
-LOG_FILE="rpc_check.log"
-MAIL_SETTINGS_FILE="mail_settings.txt"
+# Read settings from settings.txt
+source settings.txt
 
-# Load mail settings
-if [ -f "$MAIL_SETTINGS_FILE" ]; then
-    source "$MAIL_SETTINGS_FILE"
+# Generate the from address for the email
+from_address="RPC_Alert_$rpc_server_ip"
+
+# Check if an alert email has been sent within the last hour
+last_alert=$(grep -i "$from_address" rpc_check.log | tail -n 1 | awk '{print $1 " " $2}')
+last_alert_timestamp=$(date -d "$last_alert" +%s)
+current_timestamp=$(date +%s)
+time_diff=$((current_timestamp - last_alert_timestamp))
+if [ $time_diff -gt 3600 ]; then
+  # Send HTTP HEAD request and check the response
+  response=$(curl -sI "$rpc_server_ip:$rpc_port")
+  if [[ ! $response =~ "200 OK" ]]; then
+    # Send email alert
+    echo "Sending alert email..."
+    echo -e "Subject: RPC Alert\n\nThere was an issue with the RPC server at $rpc_server_ip:$rpc_port" | ssmtp "$email_address"
+    echo "$(date) - Alert email sent" >> rpc_check.log
+  else
+    echo "$(date) - RPC server is functioning properly" >> rpc_check.log
+  fi
 else
-    echo "Error: $MAIL_SETTINGS_FILE not found."
-    exit 1
+  echo "$(date) - Alert email already sent within the last hour" >> rpc_check.log
 fi
-
-# Function to send email alert
-send_alert_email() {
-    subject="RPC Alert - Issue detected with RPC on $RPC_IP_ADDRESS"
-    body="There was an issue with the RPC on $RPC_IP_ADDRESS. Please check the logs for more information."
-
-    echo -e "Subject:$subject\n$body" | ssmtp -v -s "$SMTP_HOST:$SMTP_PORT" "$EMAIL_ADDRESS_TO"
-}
-
-# Function to perform RPC check
-perform_rpc_check() {
-    # Perform HTTP HEAD request to RPC and capture the response
-    response=$(curl -s -I --connect-timeout 5 "http://$RPC_IP_ADDRESS:8989")
-
-    # Check if the response is correct
-    if [[ $response == *"200 OK"* ]]; then
-        echo "[$(date '+%Y-%m-%d %H:%M:%S')] RPC is working fine."
-    else
-        echo "[$(date '+%Y-%m-%d %H:%M:%S')] RPC is not responding properly. Sending alert email..."
-        send_alert_email
-    fi
-}
-
-# Main script
-echo "Running rpc_check.sh..."
-
-# Perform RPC check
-perform_rpc_check >> "$LOG_FILE"
-
-echo "RPC check completed."
