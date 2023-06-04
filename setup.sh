460382d3f03d85f6aa594411950492d760d714c1
s4njk4n <105919271+s4njk4n@users.noreply.github.com>
2023-06-04 18:39:06 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
-# Check if sudo is required
+# Check if running as root, otherwise run commands with sudo
 if [ "$(id -u)" -ne 0 ]; then
     SUDO="sudo"
 fi
 
 # Install dependencies
-$SUDO apt update
-$SUDO apt install -y curl postfix ufw
+$SUDO apt-get update
+$SUDO apt-get install -y curl postfix ufw
 
-# Configure Postfix
-echo "postfix postfix/main_mailer_type select Internet Site" | $SUDO debconf-set-selections
-$SUDO dpkg-reconfigure -f noninteractive postfix
+# Configure postfix as an Internet Site
+echo "postfix postfix/mailname string $(hostname)" | $SUDO debconf-set-selections
+echo "postfix postfix/main_mailer_type string 'Internet Site'" | $SUDO debconf-set-selections
+$SUDO $SUDO dpkg-reconfigure -f noninteractive postfix
 
 # Configure Postfix settings
-$SUDO postconf -e "mydestination = localhost.localdomain, localhost"
-$SUDO postconf -e "inet_interfaces = loopback-only"
-
-# Configure UFW
-$SUDO ufw allow Postfix
-$SUDO ufw deny from any to any port 25
-
-# Ask for alert email address
-read -p "Enter the email address to send alert emails to: " ALERT_EMAIL
-
-# Save settings to settings.txt
-echo "ALERT_EMAIL=$ALERT_EMAIL" >settings.txt
-echo "RPC_URL=" >>settings.txt
-
-# Restart services
+$SUDO sed -i 's/inet_interfaces = all/inet_interfaces = loopback-only/' /etc/postfix/main.cf
+$SUDO sed -i '/mydestination/ s/$/,'$(hostname)'/' /etc/postfix/main.cf
 $SUDO systemctl restart postfix
 
+# Configure UFW
+$SUDO ufw allow from 127.0.0.1 to any port 25
+$SUDO ufw deny from any to any port 25
+$SUDO ufw --force enable
+
+# Ask user for email address and RPC URL
+read -p "Enter the email address to receive alert emails: " ALERT_EMAIL
+read -p "Enter the RPC URL to check: " RPC_URL
+
+# Store user-defined data in settings.txt file
+echo "ALERT_EMAIL=$ALERT_EMAIL" > "$(dirname "$0")/settings.txt"
+echo "RPC_URL=$RPC_URL" >> "$(dirname "$0")/settings.txt"
+
 # Create rpc_check.log file
-touch rpc_check.log
+touch "$(dirname "$0")/rpc_check.log"
