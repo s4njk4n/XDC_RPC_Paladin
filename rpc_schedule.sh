2edbc690d462b77e83be02462919654188efdcc7
s4njk4n <105919271+s4njk4n@users.noreply.github.com>
2023-06-04 18:28:52 +1000

    Update rpc_schedule.sh

--- rpc_schedule.sh
+++ rpc_schedule.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 # Check if sudo is required
-if [[ $EUID -ne 0 ]]; then
+if [ "$(id -u)" -ne 0 ]; then
     SUDO="sudo"
 fi
 
 # Function to set up cron job
-setup_cron() {
-    $SUDO crontab -l | { cat; echo "* * * * * ~/XDC_RPC_Check/rpc_check.sh >> ~/XDC_RPC_Check/rpc_check.log 2>&1 &"; } | $SUDO crontab -
+setup_cron_job() {
+    $SUDO crontab -l | { cat; echo "* * * * * $HOME/XDC_RPC_Check/rpc_check.sh >/dev/null 2>&1 &"; } | $SUDO crontab -
     echo "Cron job set up successfully."
 }
 
-# Function to cancel cron job
-cancel_cron() {
-    $SUDO crontab -l | grep -v "~/XDC_RPC_Check/rpc_check.sh" | $SUDO crontab -
-    echo "Cron job canceled successfully."
+# Function to remove cron job
+remove_cron_job() {
+    $SUDO crontab -l | grep -v "$HOME/XDC_RPC_Check/rpc_check.sh" | $SUDO crontab -
+    echo "Cron job removed successfully."
 }
 
 # Interactive menu
-echo "RPC Schedule"
+echo "RPC Schedule Menu"
 echo "1. Set up minutely RPC check"
 echo "2. Cancel minutely RPC check"
-read -p "Enter your choice (1-2): " choice
+read -p "Enter your choice: " choice
 
 case $choice in
     1)
-        setup_cron
+        setup_cron_job
         ;;
     2)
-        cancel_cron
+        remove_cron_job
         ;;
     *)
-        echo "Invalid choice. Exiting..."
+        echo "Invalid choice"
         ;;
 esac
